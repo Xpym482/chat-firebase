25ab3fbd0e3cca9f500e285eecd1060d369293dc
Daniel Golubkov <43164744+dangol9@users.noreply.github.com>
2020-12-05 20:57:50 +0200

    style changes

--- js/index.js
+++ js/index.js
@@ -4,10 +4,12 @@ let email = null;
 
 function sendMessage(){
     let messageInput = document.getElementById("msg-input");
+    let text = messageInput.value;
+    if(!text.trim()) return alert('Please enter message.');
     const msg = {
         email,
         name,
-        text: messageInput.value
+        text
     };
 
     msgRef.push(msg);
@@ -17,12 +19,17 @@ function sendMessage(){
 function load(){
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
+            const userNameContainer = document.getElementById("your-name");
             const userName = document.getElementById("user-name");
             name = user.displayName;
             email = user.email;
+            photoUrl = user.photoURL;
+            photo = document.getElementById("profile-image");
+            photo.src=photoUrl;
             msgRef.on('child_added', updateMessages);
-            console.log(user);
-            userName.innerHTML = "Welcome, " + name + "!";
+            userNameContainer.innerHTML = name;
+            //userName.innerHTML = "Welcome, " + name + "!";
+
         } else {
             window.location.replace("login.html");
         }
@@ -42,8 +49,11 @@ function updateMessages(data) {
     const messagesContainer = document.getElementById("messages");
     const {email: userEmail , name, text} = data.val();
     const msg = `<li class="message" id="${email === userEmail ? "messages-sent": "messages-received"}">
-    <i class = "name">${name}: </i>${text}
+    <i class = "name">${name}</i><br><i>${text}</i>
     </li>`;
+    if(email != userEmail){
+        console.log(userEmail);
+    }
 
     messagesContainer.innerHTML += msg;
     document.getElementById("conversation").scrollTop = document.getElementById("conversation").scrollHeight;
